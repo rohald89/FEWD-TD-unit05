2a3ce126115704a8ab00bc02b2b6464bbb34d758
Rohald van Merode <rohald_89@live.nl>
2021-06-21 18:22:56 +0200

    refactored JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,35 @@
 //===========================
-//========= jQuery ==========
+//======= JAVASCRIPT ========
 //===========================
 
-const $images = $(".thumbnail");
-const $captions = $("[data-title]");
-const $input = $("#search");
+const images = document.querySelectorAll(".thumbnail");
+const captions = document.querySelectorAll("[data-title]");
+const input = document.querySelector("#search");
 
-$captions.each(function (i) {
-  let $caption = $(this).attr("data-title");
-  $input.on("keyup", () => {
-    if ($caption.indexOf($input.val().toLowerCase()) < 0) {
-      $images[i].style.display = "none";
-    } else {
-      $images[i].style.display = "";
-    }
+captions.forEach((caption, i) => {
+  let title = caption.getAttribute("data-title").toLowerCase();
+  input.addEventListener('keyup', () => {
+    title.indexOf(input.value.toLowerCase()) < 0 ? 
+    images[i].style.display = 'none' : 
+    images[i].style.display = '';
   });
 });
 
 //===========================
-//======= JAVASCRIPT ========
+//========= jQuery ==========
 //===========================
 
-// const images = document.getElementsByClassName("thumbnail");
-// const captions = document.querySelectorAll("[data-title]");
-// const input = document.getElementById("search");
+// const $images = $(".thumbnail");
+// const $captions = $("[data-title]");
+// const $input = $("#search");
 
-// for (let i = 0; i < captions.length; i++) {
-//   let caption = captions[i].getAttribute("data-title").toLowerCase();
-//   console.log(caption);
-//   input.addEventListener("keyup", () => {
-//     if (caption.indexOf(input.value.toLowerCase()) < 0) {
-//       images[i].style.display = "none";
+// $captions.each(function (i) {
+//   let $caption = $(this).attr("data-title");
+//   $input.on("keyup", () => {
+//     if ($caption.indexOf($input.val().toLowerCase()) < 0) {
+//       $images[i].style.display = "none";
 //     } else {
-//       images[i].style.display = "";
+//       $images[i].style.display = "";
 //     }
 //   });
-// }
+// });
